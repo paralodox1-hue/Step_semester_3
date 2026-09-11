7a837631dd309abbcc4a7f653e85f073c90d0c25
Dakshinamurthy S <paralodox1@gmail.com>
2026-09-11 22:11:45 +0530

    Assignment problem implemented

---
+++ feature_session3/Assignmentproblem/CompanyEmployeeDemo.java
@@ -0,0 +1,28 @@
+class Employee {
+    String empName;
+    double salary;
+
+    static String companyName = "Bright Horizon Technologies";
+    static int employeeCount = 0;
+
+    Employee(String empName, double salary) {
+        this.empName = empName;
+        this.salary = salary;
+        employeeCount++;
+    }
+
+    static void printCompanyInfo() {
+        System.out.println(companyName);
+        System.out.println("Employees on record: " + employeeCount);
+    }
+}
+
+public class CompanyEmployeeDemo {
+    public static void main(String[] args) {
+        Employee employee1 = new Employee("Divya", 65000);
+        Employee employee2 = new Employee("Arjun", 45000);
+        Employee employee3 = new Employee("Priya", 55000);
+
+        Employee.printCompanyInfo();
+    }
+}
